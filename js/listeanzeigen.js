a5c61e6a65545d779acdc464351216d33ee1222d
Schwegelboot <dome.schwegler@web.de>
2019-10-07 17:19:59 +0200

    add bootstrap

--- js/listeanzeigen.js
+++ js/listeanzeigen.js
@@ -1,8 +1,11 @@
 $(document).ready(function () {
     $('#bt1').click(function (){
         $.get("https://shopping-lists-api.herokuapp.com/api/v1/lists/5d9a21b50f5da70017537715", function(data,status){
-            $('#requestoutput').append(data.items[0].name);
-            console.log(data);
+            $('#liste').append("<h1>"+data.name+"</h1>" + "<br>");
+            data.items.forEach(element => {
+                listElement= "<p>"+element.name+"</p>";
+                $('#liste').append(listElement);
+            });
         })
     });
 })
