0e56ce212daaa1af4d541be903aef57c56141c81
Schwegelboot <dome.schwegler@web.de>
2019-10-09 14:02:49 +0200

    elemente werden nun rechts aufgelistet

--- js/listeanzeigen.js
+++ js/listeanzeigen.js
@@ -9,3 +9,20 @@ $(document).ready(function () {
         })
     });
 })
+
+$(document).ready(function () {
+        $.get("https://shopping-lists-api.herokuapp.com/api/v1/lists/5d9a21b50f5da70017537715", function(data,status){
+            $('#listen').append("<p class=listenname>"+data.name+"</p>" + "<br>");
+        })
+})
+
+$(document).ready(function () {
+    $('#openlist').click(function (){
+        $.get("https://shopping-lists-api.herokuapp.com/api/v1/lists/5d9a21b50f5da70017537715", function(data,status){
+                data.items.forEach(element => {
+                listElement= "<p>"+element.name+"</p>";
+                $('#elemente').append(listElement);
+            });
+        })
+    });
+})
