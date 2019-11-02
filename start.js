19d981de407afba3521b97dfcca1981e7f0ba383
lukasmast98 <56023174+lukasmast98@users.noreply.github.com>
2019-11-02 14:31:06 +0100

    Add files via upload

---
+++ start.js
@@ -0,0 +1,14 @@
+function addAChild () {
+    var myH2 = document.createElement('Listenelement');
+    var myText = document.createTextNode('Eine sehr dynamische Seite');
+    myH2.appendChild(myText);
+    var Ausgabebereich = document.getElementById('main');
+    Ausgabebereich.appendChild(myH2);
+  }
+
+  function init () {
+    var element  = document.getElementById ('one');
+    element.addEventListener ('click', addAChild);
+  }
+  
+  document.addEventListener('DOMContentLoaded', init);
